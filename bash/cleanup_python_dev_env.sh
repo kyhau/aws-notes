b005e5bd24da63d0bc6c0c64b6eba5a1c2b93790
Kay Hau <virtualda@gmail.com>
2019-10-01 07:43:00 +1000

    Added cleanup_python_dev_env.sh

---
+++ bash/cleanup_python_dev_env.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#echo "Folder size: $(du -sh)" .
+
+declare -a FOLDERS=(
+  "build"
+  "dist"
+  "env"
+  "env_*"
+  "wheelmaker"
+  ".cache"
+  ".pytest_cache"
+  ".tox"
+  "*.egg-info"
+  "__pycache__"
+)
+declare -a FILES=(
+  ".coverage*"
+  "coverage*.xml"
+  "flake8.log"
+  "junit-py*.xml"
+  "pylint.log"
+  "gurobi.log"
+  "pyinstaller_out.log"
+)
+
+delete_stuff() {
+  local option=${1}
+  shift
+  local targets=("$@")
+  for target in "${targets[@]}"
+  do
+    echo "Finding $target in current directory"
+    for item in $(find -name "${target}" -type ${option})
+    do
+      echo "Removing ${item}"
+      rm -rf ${item}
+    done
+  done
+}
+
+delete_stuff "d" "${FOLDERS[@]}"
+delete_stuff "f" "${FILES[@]}"
+
+#echo "Folder size: $(du -sh)" .
